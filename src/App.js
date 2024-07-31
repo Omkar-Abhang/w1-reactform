f929ed4b77f9683b13ddcf9f284bcc0a4a78454d
Omkar-Abhang <omkarabhang36@gmail.com>
2024-07-31 12:33:26 +0530

    created this simple react form for practice react component

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import {Router, Route, Routes } from 'react-router-dom';
+import Formcomponent from './components/formcomponent';
+
+
+function App() {
+  
+  return (
+
+    <>
+      <Formcomponent />
+
+   
+      
+    </>
+
+
+
+  );
+}
+
+export default App;
